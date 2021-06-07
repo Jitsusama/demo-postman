89a4c67b75a81a725504e1c6d9ec8fac797012f5
Joel Gerber <Joel.Gerber@corp.eastlink.ca>
2021-06-07 15:01:12 -0400

    Improve UI

--- ui/src/app.js
+++ ui/src/app.js
@@ -1,15 +1,27 @@
 import {useEffect, useRef, useState} from 'react'
 import style from './app.module.css'
 
+const DEFAULT_BASEURI = 'http://127.0.0.1:8080';
+
 export const App = () => {
     const [state, setState] = useState('loggedOut');
-    const [baseUri, setBaseUri] = useState('http://127.0.0.1:8080')
+    const [baseUri, setBaseUri] = useState(DEFAULT_BASEURI)
+    useEffect(() => {
+        const oldBaseUri = window.sessionStorage.getItem('baseUri')
+        if (oldBaseUri && baseUri === DEFAULT_BASEURI)
+            setBaseUri(oldBaseUri)
+        else if (!oldBaseUri || oldBaseUri !== baseUri)
+            window.sessionStorage.setItem('baseUri', baseUri)
+    }, [baseUri])
+
+    const login = () => setState('loggedIn')
+    const logout = () => setState('loggedOut')
 
     return <main className={style.app}>
         <BaseUri value={baseUri} onChange={v => setBaseUri(v)}/>
         {state === 'loggedOut'
-            ? <Login baseUri={baseUri} onLogin={() => setState('loggedIn')}/>
-            : <LastLogin baseUri={baseUri}/>}
+            ? <Login baseUri={baseUri} onLogin={login}/>
+            : <LastLogin baseUri={baseUri} onLogout={logout}/>}
     </main>;
 };
 
@@ -28,6 +40,7 @@ const Login = ({baseUri, onLogin}) => {
     const [state, setState] = useState('initial')
     useEffect(() => state === 'initial' && ref.current.focus(), [state])
 
+    const disabled = state === 'processing'
     const onSubmit = (event) => {
         event.preventDefault();
         (async () => {
@@ -47,35 +60,40 @@ const Login = ({baseUri, onLogin}) => {
         <form onSubmit={onSubmit} className={style.login}>
             <label>Username <input
                 type="text" autoComplete="username" value={username}
+                disabled={disabled}
                 onChange={e => setUsername(e.target.value)}/></label>
             <label>Password <input
                 type="password" autoComplete="password" value={password}
+                disabled={disabled}
                 onChange={e => setPassword(e.target.value)}/></label>
-            <button type="submit">Login</button>
-            <Modal enable={state === 'processing'}>
-                <Progress/>
-            </Modal>
-            <Modal enable={state === 'error'}>
-                <Error onRetry={() => setState('initial')}/>
-            </Modal>
+            <Progress state={state}/>
+            <button type="submit" disabled={disabled} className={style.button}>
+                Login
+            </button>
+            <Error state={state} onRetry={() => setState('initial')}/>
         </form>
     </section>
 }
 
-const LastLogin = ({baseUri, username}) => {
+const LastLogin = ({baseUri, username, onLogout}) => {
     const [state, setState] = useState('processing')
     const [lastLogin, setLastLogin] = useState('')
     const [retries, setRetries] = useState(0)
+    /** @type {{current:HTMLElement}} */
+    const ref = useRef()
 
     useEffect(() => {
         setState('processing');
         (async () => {
             try {
-                const response = await fetch(`${baseUri}/logins?username=${username}`)
+                const params = new URLSearchParams({username})
+                const response = await fetch(`${baseUri}/logins?${params}`)
                 if (!response.ok) setState('error')
                 else {
-                    setLastLogin((await response.json())[-1])
+                    const logins = await response.json()
+                    setLastLogin(logins.pop())
                     setState('loaded')
+                    ref.current.focus()
                 }
             } catch (e) {
                 setState('error')
@@ -84,42 +102,53 @@ const LastLogin = ({baseUri, username}) => {
     }, [baseUri, username, retries])
 
     const retry = () => setRetries(retries + 1)
+    const processing = state === 'processing'
+    const error = state === 'error'
     const formatter = new Intl.DateTimeFormat('en', {
         year: 'numeric', month: 'long', day: 'numeric',
         hour: 'numeric', minute: 'numeric', second: 'numeric'
     })
 
-    switch (state) {
-        case 'processing':
-            return <section><Progress/></section>
-        case 'error':
-            return <section><Error onRetry={retry}/></section>
-        default: {
-            const date = new Date(lastLogin)
-            return <section><p>{formatter.format(date)}</p></section>
-        }
-    }
+    return <section tabIndex="-1" ref={ref} className={style.lastLogin}>
+        <h1>Last Login</h1>
+        {lastLogin ? <p>{formatter.format(Date.parse(lastLogin))}</p> : null}
+        <Progress state={state}/>
+        <button type="button" onClick={retry} disabled={processing || error}
+                className={style.button}>
+            Refresh
+        </button>
+        <button type="button" onClick={onLogout} disabled={processing || error}
+                className={style.button}>
+            Logout
+        </button>
+        <Error state={state} onRetry={retry}/>
+    </section>
 }
 
-const Modal = ({enable, children}) => enable
-    ? <aside className={style.modal}>{children}</aside>
-    : null
+const Progress = ({state}) => {
+    if (state !== 'processing') return null
 
-const Progress = () => (
-    <div>
+    return <div>
         <div role="progressbar" className={style.progress}>
             <div/>
         </div>
     </div>
-)
+}
 
-const Error = ({onRetry}) => {
+const Error = ({state, onRetry}) => {
     /** @type {{current:HTMLElement}} */
-    const ref = useRef();
-    useEffect(() => ref.current.focus(), []);
+    const ref = useRef()
+    useEffect(() => ref.current?.focus(), [])
 
-    return <div className={style.error}>
-        <p>An error occurred while processing your login request.</p>
-        <button ref={ref} type="button" onClick={() => onRetry()}>Retry</button>
-    </div>
+    if (state !== 'error') return null
+
+    return <aside className={style.modal}>
+        <div tabIndex="-1" ref={ref} className={style.error}>
+            <p>An error occurred while processing your login request.</p>
+            <button type="button" className={style.button}
+                    onClick={() => onRetry()}>
+                Retry
+            </button>
+        </div>
+    </aside>
 }
